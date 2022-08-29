3e81dca80de57bc64de73c7af8b8f99f032de28e
GuillerminaS <guillerminasalinas.ok@gmail.com>
2022-08-29 18:41:09 -0300

    Create: Vista y logica para entidad Course

---
+++ Pages/CoursesView/CreateCourse.cshtml.cs
@@ -0,0 +1,40 @@
+using CRUD_NetRazor.DBContext;
+using CRUD_NetRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CRUD_NetRazor.Pages.CoursesView
+{
+    public class CreateCourseModel : PageModel
+    {
+        private readonly AppDBContext _context;
+
+        public CreateCourseModel(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Course Course { get; set; } 
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            Course.Creation_date = DateTime.Now;
+
+            _context.Add(Course);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("Index");
+
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
